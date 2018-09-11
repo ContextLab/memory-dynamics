70037241219a178a8041ccb763a49867ecf42297
student <Student@ebbinghaus.kiewit.dartmouth.edu>
2018-09-11 16:59:37 -0400

    progress buttons, typos

--- exp/static/js/experiment.js
+++ exp/static/js/experiment.js
@@ -31,13 +31,13 @@ var runExperiment = function() {
   // instructions
   var recall_instructions = {
       type: "instructions",
-      pages: ["add instructions here.",  "add another page here"];
+      pages: ["add instructions here.",  "add another page here"],
       show_clickable_nav: true
   };
 
   // recall
   var recall = {
-        type: 'free-recall',
+        type: "free-recall",
         stimulus: "<p class='mic'><i class='fa fa-microphone blink_me'></i></p>",
         stim_duration: record_time * 1000,
         trial_duration: record_time * 1000 + 2000,
@@ -53,7 +53,8 @@ var runExperiment = function() {
                     console.log('Data saved!')
                 }
             })
-  }}
+    }
+  }
 
   // initialize
   jsPsych.init({
