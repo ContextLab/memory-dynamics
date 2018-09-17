b2be9f49a1397401207fce29a24bd4ce1a86e71d
student <Student@ebbinghaus.kiewit.dartmouth.edu>
2018-09-17 19:52:52 -0400

    fixed typo

--- exp/static/js/demographics.js
+++ exp/static/js/demographics.js
@@ -1,5 +1,5 @@
 var runDemographics = function(options, cb) {
-      $("body").append("<div id='googleform'><iframe src='https://docs.google.com/forms/d/e/1FAIpQLSc-JwH2sJM-iAtnrMyw-Gg-QYWxdvhWmOKLeQMYMFvJ04uSjA/viewform?embedded=true' width=‘" +
+      $("body").append("<div id='googleform'><iframe src='https://docs.google.com/forms/d/e/1FAIpQLSc-JwH2sJM-iAtnrMyw-Gg-QYWxdvhWmOKLeQMYMFvJ04uSjA/viewform?embedded=true' width='" +
           $(window).width() + "' height='" +
           $(window).height() + "' frameborder='0' marginheight='0' marginwidth='0'>Loading...</iframe></div>")
 
