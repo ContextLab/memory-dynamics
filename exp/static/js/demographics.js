53b3f120d36bf07bc54b888556687b3a442bf2da
student <Student@ebbinghaus.kiewit.dartmouth.edu>
2018-09-18 03:13:32 -0400

    fixed navigation through recall

--- exp/static/js/demographics.js
+++ exp/static/js/demographics.js
@@ -6,10 +6,13 @@ var runDemographics = function(options, cb) {
 
       // $("body").append("<div id='googleform'><<iframe src='https://docs.google.com/forms/d/e/1FAIpQLSc-JwH2sJM-iAtnrMyw-Gg-QYWxdvhWmOKLeQMYMFvJ04uSjA/viewform?embedded=true' width="640" height="2337" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>")
 
-      $("body").append("<button class='btn btn-success' id='nextBtn' style='position: fixed; bottom: 0px; right: 0px;' id='fixedbutton'>Done</button>")};
+      $("body").append("<button class='btn btn-success' id='nextBtn' style='position: fixed; bottom: 0px; right: 0px;' id='fixedbutton'>Done</button>")
+
 
       $('#nextBtn').click(function() {
-          $("body").remove();
-          cb();
+        $("body").append('testtesttesttest')
+        $("body").remove();
+        cb();
       });
     };
+  };
