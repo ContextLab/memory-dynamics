47665be17f9f4497b07789eb15a13e85b926c45e
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2018-09-19 14:04:20 -0400

    code cleanup

--- exp/static/js/run.js
+++ exp/static/js/run.js
@@ -1,12 +1,8 @@
 var options = {
-  show: true//,
-  //mode: mode
+  show: true
 };
-// runDemographics(options, function() {
-//   runStim_select(options, function() {
-//     runExperiment(options, function() {
-//       runPostQuestionnaire(options);
-//     });
-//   });
-// });
+
+// experiment initiates here. Subsequent functions are called individually within previous
+// function's defintion for variable assignment timing. Experiment timeline is:
+// demographics --> stim_select --> experiment --> postqustionnaire
 runDemographics(options)
