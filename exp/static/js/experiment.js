787675aaafe31f8083564a1f26035444992428d1
student <Student@ebbinghaus.kiewit.dartmouth.edu>
2018-09-20 20:32:47 -0400

    updated instructions

--- exp/static/js/experiment.js
+++ exp/static/js/experiment.js
@@ -1,7 +1,7 @@
 var exptimeline = []
-var recall_time = 10 // 10 minutes
-var predict_time = 10 // 5 minutes
-var exit_time = 5
+var recall_time = 1800 // 30 minutes
+var predict_time = 300 // 5 minutes
+var exit_time = 600 // 10 minutes
 
 var runExperiment = function(options, conditions) {
 
@@ -12,9 +12,11 @@ var runExperiment = function(options, conditions) {
             "<p>In this experiment, you will <strong>watch</strong> a 20-25 minute episode of a TV show and <strong>recall</strong> what happened in as much detail as possible.</p>" +
             "<p>Press the spacebar to continue.</p></div>",
             "<div class='instructions'> <p> When the episode ends, you will see a red microphone icon (<i style='color:red' class='fa fa-microphone'></i>).  This indicates that the computer has started recording.</p>" +
-            "<p>From that point on, you will have <strong>10 minutes</strong> to <strong>recall</strong> out loud the episode as fully as you can.</p>" +
+            "<p>When you see the <i style='color:red' class='fa fa-microphone'></i>, <strong>recall</strong> the episode out loud as fully as you can.</p>" +
             "<p>Press the spacebar to continue.</p></div>",
-            "<div class='instructions'> <p> Do your best to recount the events of the video in order using the characters' names, but if you realize you skipped something, feel free to go back and describe it.</p>" +
+            "<div class='instructions'> <p>Do your best to recount the events of the episode in order, using the characters' names and as much detail as you can remember, but if you realize you skipped something, feel free to go back and describe it.</p>" +
+            "<p>Press the spacebar to continue.</p></div>",
+            "<div class='instructions'> <p>You will have <strong>30 minutes</strong> to recall the episode, but if you feel you can't remember any more, you may stop after <strong>10 minutes</strong> by pressing the spacebar.</p>" +
             "<p>Press the spacebar to continue.</p></div>",
             "<div class='instructions'> <p>After that, you'll be asked to <strong>predict</strong> what you think will happen in the next episode.</p>" +
             "<p>When you see a blue microphone icon (<i style='color:blue' class='fa fa-microphone'></i>), you will have <strong>5 minutes</strong> to <strong>predict</strong> what will happen.</p>" +
@@ -31,14 +33,18 @@ var runExperiment = function(options, conditions) {
       type: "instructions",
       pages: ["<div class='instructions'> <p style='font-weight:bold'> PLEASE READ THESE INSTRUCTIONS CAREFULLY </p>" +
             "<p>In this experiment, you will start by <strong>recalling</strong> the episode you watched when you were last here.</p>" +
-            "<p>When you see a green microphone icon (<i style='color:green' class='fa fa-microphone'></i>), you will have <strong>10 minutes</strong> to <strong>recall</strong> the episode to the best of your ability.</p>" +
+            "<p>When you see a green microphone icon (<i style='color:green' class='fa fa-microphone'></i>), <strong>recall</strong> the episode to the best of your ability.</p>" +
+            "<p>Press the spacebar to continue.</p></div>",
+            "<div class='instructions'> <p>You will have <strong>30 minutes</strong> to recall the episode, but if you feel you can't remember any more, you may stop after <strong>10 minutes</strong> by pressing the spacebar.</p>" +
             "<p>Press the spacebar to continue.</p></div>",
             "<div class='instructions'> <p>You will then <strong>watch</strong> a 20-25 minute episode of a TV show and <strong>recall</strong> what happened in as much detail as possible.</p>" +
             "<p>Press the spacebar to continue.</p></div>",
             "<div class='instructions'> <p> When the episode ends, you will see a red microphone icon (<i style='color:red' class='fa fa-microphone'></i>).</p>" +
-            "<p>From that point on, you will have <strong>10 minutes</strong> to <strong>recall</strong> the episode as fully as you can.</p>" +
+            "<p>When you see the <i style='color:red' class='fa fa-microphone'></i>, <strong>recall</strong> the episode as fully as you can.</p>" +
             "<p> Press the spacebar to continue.</p></div>",
-            "<div class='instructions'> <p> Do your best to recount the events of the video in order using the characters' names, but if you realize you skipped something, feel free to go back and describe it.</p>" +
+            "<div class='instructions'> <p>Do your best to recount the events of the episode in order, using the characters' names and as much detail as you can remember, but if you realize you skipped something, feel free to go back and describe it.</p>" +
+            "<p>Press the spacebar to continue.</p></div>",
+            "<div class='instructions'> <p>You will have <strong>30 minutes</strong> to recall the episode, but if you feel you can't remember any more, you may stop after <strong>10 minutes</strong> by pressing the spacebar.</p>" +
             "<p>Press the spacebar to continue.</p></div>",
             "<div class='instructions'> <p>Okay that's everything! Ready to start?</p>" +
             "<p><strong>When you're ready to recall the last episode, press the spacebar.</strong></p></div>"
@@ -127,7 +133,7 @@ var runExperiment = function(options, conditions) {
   // instructions for next episode prediction (1st run only)
   var predict_instructions = {
       type: "instructions",
-      pages: ["<div class='instructions'><p>Now, when you see the <i style='color:blue' class='fa fa-microphone'></i>, pedict what will happen in the next episode to the best of your ability.</p>" +
+      pages: ["<div class='instructions'><p>Now, when you see the <i style='color:blue' class='fa fa-microphone'></i>, predict what will happen in the next episode to the best of your ability.</p>" +
       "<p>Please remember to speak <strong>clearly</strong>.</p>" +
       "<p><strong>When you're ready to begin predicting, press the spacebar.</strong></p></div>"
       ],
