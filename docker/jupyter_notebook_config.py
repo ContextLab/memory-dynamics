26b066e07b2f644943519389d437ae2b6787e540
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-09-26 19:00:52 -0400

    set server-related options on ServerApp instead of deprecated NotebookApp

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -7,13 +7,16 @@ based on arguments passed during build.
 from os import getenv
 
 
+# server config options
+c.ServerApp.ip = getenv("NOTEBOOK_IP")
+c.ServerApp.port = int(getenv("NOTEBOOK_PORT"))
+c.ServerApp.root_dir = getenv("NOTEBOOK_DIR")
+c.ServerApp.open_browser = False
+c.ServerApp.allow_root = True
+
+# notebook app config options
 c.Completer.use_jedi = False
 c.IPCompleter.use_jedi = False
-c.NotebookApp.ip = getenv("NOTEBOOK_IP")
-c.NotebookApp.port = int(getenv("NOTEBOOK_PORT"))
-c.NotebookApp.notebook_dir = getenv("NOTEBOOK_DIR")
-c.NotebookApp.open_browser = False
-c.NotebookApp.allow_root = True
 c.NotebookApp.show_banner = False
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
