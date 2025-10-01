385efcd1ffbf2480946a63297ca59a36fa3f44ad
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-10-01 19:11:37 -0400

    update some data paths in container

--- code/analysis-helpers/analysis_helpers/constants.py
+++ code/analysis-helpers/analysis_helpers/constants.py
@@ -8,8 +8,10 @@ DATA_DIR = Path('/mnt/data')
 
 RAW_DIR = DATA_DIR.joinpath('raw')
 ANNOTATIONS_DIR = RAW_DIR.joinpath('annotations')
-TRANSCRIPTIONS_DIR = RAW_DIR.joinpath('transcriptions', 'manual')
+GOOGLE_FORM_DIR = RAW_DIR.joinpath('google-form-data')
+PSITURK_DIR = RAW_DIR.joinpath('psiturk')
+TRANSCRIPTIONS_DIR = RAW_DIR.joinpath('transcriptions')
 
 PROCESSED_DIR = DATA_DIR.joinpath('processed')
-EPISODE_DATA_DIR = PROCESSED_DIR.joinpath('models', 'episodes')
-RECALL_DATA_DIR = PROCESSED_DIR.joinpath('models', 'recalls')
+EPISODE_DATA_DIR = PROCESSED_DIR.joinpath('episodes')
+RECALL_DATA_DIR = PROCESSED_DIR.joinpath('recalls')
