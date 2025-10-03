0d9430bd5f9e54e8ab316bd1a47088bccaf35fa3
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-10-02 22:11:13 -0400

    add timestamps of last video frame to use for interpolating trajectories

--- code/analysis-helpers/analysis_helpers/constants.py
+++ code/analysis-helpers/analysis_helpers/constants.py
@@ -15,3 +15,14 @@ TRANSCRIPTIONS_DIR = RAW_DIR.joinpath('transcriptions')
 PROCESSED_DIR = DATA_DIR.joinpath('processed')
 EPISODE_DATA_DIR = PROCESSED_DIR.joinpath('episodes')
 RECALL_DATA_DIR = PROCESSED_DIR.joinpath('recalls')
+
+
+########################################################################
+#                      TOPIC MODELING PARAMETERS                       #
+########################################################################
+# timestamp of last video frame, used for interpolating timeseries
+ENDFRAME_TIMES = {
+    'atlep1': 1466.0,
+    'atlep2': 1316.52,
+    'arrdev': 1236.6
+}
