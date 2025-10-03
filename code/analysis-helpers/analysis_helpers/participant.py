ab8e9d875368041acc61abb158a1a16fb2ee67a5
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-10-02 22:10:25 -0400

    Participant class to provide access to individual participant data

--- code/analysis-helpers/analysis_helpers/participant.py
+++ code/analysis-helpers/analysis_helpers/participant.py
@@ -1,2 +1,125 @@
-class Participant:
-    ...
+from __future__ import annotations
+
+from functools import cached_property
+from typing import ClassVar, Self
+
+import pandas as pd
+
+from analysis_helpers.constants import PROCESSED_DIR, TRANSCRIPTIONS_DIR
+from analysis_helpers.internals import cached_classproperty, LazyDataDict, Multiton
+
+
+class Participant(metaclass=Multiton):
+    """
+    Class that represents an individual participant and provides access
+    to their data via attributes. Data sources are lazily loaded and
+    cached on first access to keep instances as lightweight as possible.
+    """
+    id_mapping: ClassVar[pd.DataFrame] = pd.read_csv(
+            PROCESSED_DIR.joinpath('subid-mapping.csv'),
+            index_col='Subject ID',
+            dtype_backend='numpy_nullable'
+    )
+
+    @classmethod
+    def from_subid(cls, subid: str) -> Self:
+        """
+        Factory method for creating Participant instances from Subject
+        IDs.
+
+        Parameters
+        ----------
+        subid : str
+            The participant's Subject ID (beginning with "MD-").
+
+        Returns
+        -------
+        Participant
+            A new instance of the Participant class.
+        """
+        try:
+            sub_n = cls.id_mapping.index.get_loc(subid) + 1
+        except KeyError as e:
+            raise ValueError(
+                    f'No participant found with Subject ID "{subid}"'
+            ) from e
+        return cls(sub_n)
+
+    @classmethod
+    def load_all(cls) -> tuple[Participant, ...]:
+        """
+        Create instances for all participants and return them as a tuple.
+
+        Returns
+        -------
+        tuple of Participant
+        """
+        return tuple(cls(n) for n in range(1, cls.id_mapping.shape[0] + 1))
+
+    def __init__(self, sub_n: int) -> None:
+        """
+        Main constructor for the Participant class.
+
+        Creates an object that provides access to an individual
+        participant's data given their numeric index (see below). To
+        create Participant instances directly from subject IDs, use the
+        `Participant.from_subid()` factory method.
+
+        Parameters
+        ----------
+        sub_n : int
+            The **1-indexed** row-index of the participant in
+            `subid-mapping.csv` (corresponds to the order in which
+            participants were collected).
+        """
+        if sub_n not in range(1, Participant.id_mapping.shape[0] + 1):
+            raise ValueError(
+                    'Participant indices range from 1 to '
+                    f'{Participant.id_mapping.shape[0]} (inclusive).'
+            )
+        id_mapping_row = Participant.id_mapping.iloc[sub_n - 1]
+        self.sub_n = sub_n
+        self.subid = id_mapping_row.name
+        self.ses1_id = id_mapping_row['session 1']
+        self.ses2_id = id_mapping_row['session 2']
+        self.condition = self.subid.split('-')[2]
+
+        self.transcripts = LazyDataDict(self, {
+            'atlep1': 'atlep1_recall_transcript',
+            'delayed': 'delayed_recall_transcript',
+            'atlep2': 'atlep2_recall_transcript',
+            'arrdev': 'arrdev_recall_transcript'
+        })
+
+    @cached_property
+    def atlep1_recall_transcript(self) -> str:
+        return TRANSCRIPTIONS_DIR.joinpath(
+                self.subid, self.ses1_id, f'{self.ses1_id}-recall.txt'
+        ).read_text()
+
+    @cached_property
+    def delayed_recall_transcript(self) -> str:
+        return TRANSCRIPTIONS_DIR.joinpath(
+                self.subid, self.ses2_id, f'{self.ses2_id}-delayed.txt'
+        ).read_text()
+
+    @cached_property
+    def atlep2_recall_transcript(self) -> str:
+        if self.condition == 'B':
+            raise AttributeError(
+                    f'Condition B participant "{self.subid}" did not view atlep2'
+            )
+        return TRANSCRIPTIONS_DIR.joinpath(
+                self.subid, self.ses2_id, f'{self.ses2_id}-recall.txt'
+        ).read_text()
+
+    @cached_property
+    def arrdev_recall_transcript(self) -> str:
+        if self.condition == 'A':
+            raise AttributeError(
+                    f'Condition A participant "{self.subid}" did not view arrdev'
+            )
+        return TRANSCRIPTIONS_DIR.joinpath(
+                self.subid, self.ses2_id, f'{self.ses2_id}-recall.txt'
+        ).read_text()
+
