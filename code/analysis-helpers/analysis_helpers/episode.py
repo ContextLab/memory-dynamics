082030941b8013d3352d2131b4cd33dc917735d8
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-10-02 22:10:52 -0400

    add access to episode annotations

--- code/analysis-helpers/analysis_helpers/episode.py
+++ code/analysis-helpers/analysis_helpers/episode.py
@@ -1,2 +1,23 @@
-class Episode:
-    ...
+from __future__ import annotations
+
+from functools import cached_property
+from typing import Literal
+
+import pandas as pd
+
+from analysis_helpers.constants import ANNOTATIONS_DIR, ENDFRAME_TIMES
+from analysis_helpers.internals import Multiton
+
+
+class Episode(metaclass=Multiton):
+    # ADD DOCSTRING
+    def __init__(self, name: Literal['atlep1', 'atlep2', 'arrdev']) -> None:
+        if name not in {'atlep1', 'atlep2', 'arrdev'}:
+            raise ValueError(f"Invalid episode name: {name}")
+        self.name = name
+        self.endframe_time = ENDFRAME_TIMES[name]
+
+    @cached_property
+    def annotations(self) -> pd.DataFrame:
+        return pd.read_csv(ANNOTATIONS_DIR.joinpath(f'{self.name}.csv'),
+                           dtype_backend='numpy_nullable')
