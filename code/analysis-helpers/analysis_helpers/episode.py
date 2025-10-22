69e3adbc39ba7f376e538b4d8e5a29e22c4968b8
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2025-10-22 19:06:11 -0400

    add data accessors for fit models, episode trajectories, windows

--- code/analysis-helpers/analysis_helpers/episode.py
+++ code/analysis-helpers/analysis_helpers/episode.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
+import pickle
 from functools import cached_property
 from typing import Literal
 
+import numpy as np
 import pandas as pd
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
 
-from analysis_helpers.constants import ANNOTATIONS_DIR, ENDFRAME_TIMES
+from analysis_helpers.constants import (
+    ANNOTATIONS_DIR, 
+    ENDFRAME_TIMES,
+    EPISODE_DATA_DIR
+)
 from analysis_helpers.internals import Multiton
 
 
@@ -16,8 +24,25 @@ class Episode(metaclass=Multiton):
             raise ValueError(f"Invalid episode name: {name}")
         self.name = name
         self.endframe_time = ENDFRAME_TIMES[name]
+        self.data_dir = EPISODE_DATA_DIR.joinpath(name)
 
     @cached_property
     def annotations(self) -> pd.DataFrame:
         return pd.read_csv(ANNOTATIONS_DIR.joinpath(f'{self.name}.csv'),
                            dtype_backend='numpy_nullable')
+
+    @cached_property
+    def fit_cv(self) -> CountVectorizer:
+        return pickle.loads(self.data_dir.joinpath('fit_cv.p').read_bytes())
+    
+    @cached_property
+    def fit_lda(self) -> LatentDirichletAllocation:
+        return pickle.loads(self.data_dir.joinpath('fit_lda.p').read_bytes())
+    
+    @cached_property
+    def trajectory(self) -> np.ndarray:
+        return np.load(self.data_dir.joinpath('trajectory.npy'))
+
+    @cached_property
+    def windows(self) -> list[str]:
+        return np.load(self.data_dir.joinpath('windows.npy'))
