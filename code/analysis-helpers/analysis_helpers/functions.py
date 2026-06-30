869cf44a63bd9bb2f39b044e578b5f29035671af
paxtonfitzpatrick <Paxton.C.Fitzpatrick@Dartmouth.edu>
2026-06-30 13:07:44 -0400

    function for mean-centering multiple trajectories by collective mean centroid

--- code/analysis-helpers/analysis_helpers/functions.py
+++ code/analysis-helpers/analysis_helpers/functions.py
@@ -1,6 +1,7 @@
 import re
 
 import matplotlib.pyplot as plt
+import numpy as np
 from IPython.display import display, Markdown
 from matplotlib import font_manager
 
@@ -8,9 +9,32 @@ from analysis_helpers.constants import CONTENT_WARNING, FONTS_DIR
 from analysis_helpers.internals import _imported_from_notebook
 
 
+def mean_center(*to_center, equal_weight=True):
+    """
+    Mean-center one or more feature matrices by their shared centroid.
+
+    Returns a tuple of inputs with the same shape & order as passed.
+    """
+    if len(to_center) == 1:
+        return to_center[0] - to_center[0].mean(axis=0)
+
+    input_was_1d = [x.ndim == 1 for x in to_center]
+    to_center = [np.atleast_2d(x) for x in to_center]
+    if equal_weight:
+        mean_centroid = np.mean([x.mean(axis=0) for x in to_center], axis=0)
+        centered = [x - mean_centroid for x in to_center]
+    else:
+        split_inds = np.cumsum([x.shape[0] for x in to_center])[:-1]
+        stacked = np.vstack(to_center)
+        stacked = stacked - stacked.mean(axis=0)
+        centered = np.vsplit(stacked, split_inds)
+
+    return tuple(c[0] if was_1d else c for c, was_1d in zip(centered, input_was_1d))
+
+
 def set_figure_style():
     """
-    Sets some helpful `matplotlib` options for figures generated for the 
+    Sets some helpful `matplotlib` options for figures generated for the
     paper. This gets called automatically whenever `analysis_helpers` is
     imported, but occasionally needs to be called again manually when
     some other function has overwritten the relevant
@@ -24,7 +48,7 @@ def set_figure_style():
     if FONTS_DIR.is_dir() and next(FONTS_DIR.iterdir(), None) is not None:
         # check whether the font has already been loaded by
         # `matplotlib`'s font manager
-        # TODO: this is imperfect and only loads all Myriad Pro styles 
+        # TODO: this is imperfect and only loads all Myriad Pro styles
         #  from FONTS_DIR if *no* Myriad Pro font is already loaded
         myriad_pro_fonts = [
             f for f in font_manager.fontManager.ttflist if f.name == 'Myriad Pro'
